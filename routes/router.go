4a0b259d073c6e3d5515c0939fb262fa9e107dc9
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-12 12:13:22 +0700

    add middleware

--- routes/router.go
+++ routes/router.go
@@ -6,7 +6,16 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func middleware(c *fiber.Ctx) error {
+	token := c.Get("token")
+	if token != "RAHASIA" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"status": "UNAUTHORIZED",
+		})
+	}
+	return c.Next()
+}
 func Route(app *fiber.App, controller controller.UserController) {
-	app.Get("/api/users", controller.FindAll)
-	app.Post("/api/users", controller.Insert)
+	app.Get("/api/users", middleware, controller.FindAll)
+	app.Post("/api/users", middleware, controller.Insert)
 }
