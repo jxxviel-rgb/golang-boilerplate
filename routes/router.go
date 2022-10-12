acada78030504a78cb7b00526b1f9d99d22c890d
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-12 12:34:03 +0700

    move middleware to another file

--- routes/router.go
+++ routes/router.go
@@ -2,20 +2,12 @@ package routes
 
 import (
 	"mangojek-backend/controller"
+	"mangojek-backend/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func middleware(c *fiber.Ctx) error {
-	token := c.Get("token")
-	if token != "RAHASIA" {
-		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"status": "UNAUTHORIZED",
-		})
-	}
-	return c.Next()
-}
 func Route(app *fiber.App, controller controller.UserController) {
-	app.Get("/api/users", middleware, controller.FindAll)
-	app.Post("/api/users", middleware, controller.Insert)
+	app.Get("/api/users", middleware.AuthMiddleware, controller.FindAll)
+	app.Post("/api/users", middleware.AuthMiddleware, controller.Insert)
 }
