b0332fab3fd176fcc8ff9e23036962092e20eab3
Fikri Ilhamsyah <fiqriilhamsyah09@gmail.com>
2022-10-12 11:43:41 +0700

    move app config to another file and wrapped it

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
 package main
 
 import (
-	"mangojek-backend/config"
-	"mangojek-backend/controller"
+	"mangojek-backend/app"
 	"mangojek-backend/exception"
-	"mangojek-backend/repository"
-	"mangojek-backend/service"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
-	configuration := config.NewConfiguration()
-	database := config.NewPostgresDatabase(configuration)
-	config.NewRunMigration(database)
-
-	// Setup Repository
-	userRepository := repository.NewUserRepositoryImpl(database)
-
-	// Setup Service
-	userService := service.NewUserServiceImpl(userRepository)
-
-	// Setup Controller
-	userController := controller.NewUserControllerImpl(userService)
-
-	// Setup Fiber
-	// app := fiber.New(config.NewFiberConfig())
-	app := fiber.New(config.NewFiberConfig())
-	app.Use(recover.New())
-
-	// Setup Routing
-	userController.Route(app)
-
+	app := app.InitializedApp()
 	// Start App
 	err := app.Listen(":3000")
 	exception.PanicIfNeeded(err)
